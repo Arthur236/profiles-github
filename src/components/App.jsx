5538f4d7ae0eca473259cfb108300e4e5720e937
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2017-10-15 12:20:56 +0300

    Setting up default App props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      	MY APP
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: 'Arthur236',
+			userData: [],
+			userRepos: [],
+			perPage: 5
+		}
+	}
+
+  	render() {
+    	return (
+	      <div>
+	      	{this.state.username}
+	      </div>
+	    );
+	}
 }
+
+App.propTypes = {
+	clientId: PropTypes.string,
+	clientSecret: PropTypes.string
+};
+
+App.defaultProps = {
+	clientId: '9aa2b856f954814b7fd7',
+	clientSecret: 'da29be686c554c8b5f6824b53cf042294d6046b5'
+};
 
 export default App;
